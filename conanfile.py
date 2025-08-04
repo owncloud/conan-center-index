6c06a51469e0285c947d8bd718a061d7be34afbd
Thomas Müller <1005065+DeepDiver1975@users.noreply.github.com>
2025-08-04 17:58:55 +0200

    feat: require extra-cmake-modules/6.8.0

--- conanfile.py
+++ conanfile.py
@@ -17,7 +17,7 @@ class GlobalDependencyMirror(ConanFile):
     }
 
     def requirements(self):
-        self.requires("extra-cmake-modules/6.2.0")
+        self.requires("extra-cmake-modules/6.8.0")
         self.requires("zlib/1.3.1")
         self.requires("sqlite3/3.49.1")
         self.requires("openssl/3.4.1")
